393238ea2d99e1d984de47741a469cb5a14443bc
hapsatidi <HMahamadou.kk@ilimi.edu.ne>
2025-02-21 22:39:16 +0100

    Initial commit  apres la reprise du travail

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from './Pages/HomeScreen';
+import AddNoteScreen from './Pages/AddNoteScreen';
+import NoteDetailScreen from'./Pages/NotesDetailScreen';
+
+
+const Stack = createStackNavigator();
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#4CAF50",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Notes & Tasks" }}
+        />
+        <Stack.Screen
+          name="AddNote"
+          component={AddNoteScreen}
+          options={{ title: "Nouvelle Note" }}
+        />
+        <Stack.Screen
+          name="NoteDetail"
+          component={NoteDetailScreen}
+          options={{ title: "Détails" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+ );
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
